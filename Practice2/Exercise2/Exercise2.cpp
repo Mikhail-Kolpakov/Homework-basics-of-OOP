181fe53a9e913607c0dd70b020f9a967c2da1ff6
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-09-12 00:20:45 +0300

    Pr. 3 Ex. 1

---
+++ Practice2/Exercise2/Exercise2.cpp
@@ -0,0 +1,60 @@
+﻿//Варіант #9
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+#include <iomanip>
+#define N 5
+#define M 4
+
+using namespace std;
+
+enum class TypeOfArray {
+    X = 'X',
+    Y = 'Y'
+};
+
+void input(int[], int, TypeOfArray);
+void calculating(int[], int, float&, float&, float&, float(float), float(float), float(float), TypeOfArray);
+
+int main() {
+    int x[N], y[M];
+    float A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, fita;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    input(x, N, TypeOfArray::X);
+    cout << endl;
+    input(y, M, TypeOfArray::Y);
+
+    calculating(x, N, A, B, C, sin, cos, tan, TypeOfArray::X);
+    calculating(y, M, D, E, F, tan, exp, cos, TypeOfArray::Y);
+
+    fita = (A * sin(B) + B * cos(C)) / (D + E * sin(F));
+
+    cout << endl << "Після проведення розрахунків fita = " << fixed << setprecision(2) << fita << endl;
+}
+
+void input(int array[], int length, TypeOfArray type_of_array) { //Функція для заповнення масиву числами з клавіатури
+    cout << "Введіть значення елементів для масиву " << static_cast<char>(type_of_array) << ": " << endl;
+    for (int i = 0; i < length; i++) {
+        cout << "Введіть значення для елементу [" << i << "]: ";
+        cin >> array[i];
+    }
+}
+
+void calculating(int array[], int length, float& f_par, float& s_par, float& t_par, float f_for(float), float s_for(float), float t_for(float), TypeOfArray type_of_array) { //Функція для обрахунку трьох вхідних параметрів
+    float s = 0, p = 1;
+    for (int i = 0; i < length; i++) {
+        s += array[i];
+        p *= array[i];
+        f_par += array[i] * f_for(array[i]);
+        s_par += array[i] * s_for(array[i]);
+        t_par += array[i] * t_for(array[i]);
+    }
+    if (type_of_array == TypeOfArray::X) {
+        f_par *= p;
+        s_par *= s;
+        t_par *= p + s;
+    }
+}
---
+++ Practice3/Exercise1/Exercise1.cpp
@@ -0,0 +1,84 @@
+﻿//Варіант #9
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#include <random>
+
+using namespace std;
+
+enum class TypeOfArray {
+    X = 'X',
+    Y = 'Y'
+};
+
+void input(float[], int, TypeOfArray);
+void print_values(float[], int, bool, TypeOfArray);
+float calculating(float[], int);
+
+int main()
+{
+    float* x, *y;
+    bool is_changed = false;
+
+    //Створюємо генератор випадкових чисел
+    random_device rand; //Створюємо джерело ентропії
+    mt19937 gen(rand()); //Створюємо об'єкт генератора випадкових чисел Mersenne Twister
+    uniform_int_distribution<int> dist(3, 20); //Задаємо діапазон для генерування чисел (2 < N < 20)
+    int N = dist(gen); //Генеруємо випадкове число
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Масиви A та B матимуть розмір(випадково згенерований): " << N << endl;
+
+    x = new float[N];
+    y = new float[N];
+
+    input(x, N, TypeOfArray::X);
+    input(y, N, TypeOfArray::Y);
+
+    cout << endl << "Масиви після введення значень: ";
+    print_values(x, N, is_changed, TypeOfArray::X);
+    print_values(y, N, is_changed, TypeOfArray::Y);
+
+    cout << endl << endl << "Найбільше значення у масиві X: " << calculating(x, N) << endl;
+    cout << "Найбільше значення у масиві Y: " << calculating(y, N) << endl;
+
+    is_changed = true;
+
+    cout << endl << "Масиви після виконання необхідних розрахунків: ";
+    print_values(x, N, is_changed, TypeOfArray::X);
+    print_values(y, N, is_changed, TypeOfArray::Y);
+
+    cout << endl;
+}
+
+void input(float array[], int length, TypeOfArray type_of_array) { //Функція для заповнення масиву числами з клавіатури
+    cout << endl << "Введіть значення елементів для масиву " << static_cast<char>(type_of_array) << ": " << endl;
+    for (int i = 0; i < length; i++) {
+        cout << "Введіть значення для елементу [" << i << "]: ";
+        cin >> array[i];
+    }
+}
+
+void print_values(float array[], int length, bool is_changed , TypeOfArray type_of_array) { //Функція для виводу вмісту масивів
+    cout << endl << "Значення масиву " << static_cast<char>(type_of_array) << ": " << endl;
+    for (int i = 0; i < length; i++) {
+        if (!is_changed)
+            cout << array[i] << " ";
+        else
+            cout << fixed << setprecision(2) << array[i] << " ";
+    }
+}
+
+float calculating(float array[], int length) { //Функція для проведення необхідних розрахунків
+    float max = array[0];
+
+    for (int i = 1; i < length; i++) //Пошук найбільшого значення у масиві
+        if (array[i] > max)
+            max = array[i]; 
+    for (int i = 0; i < length; i++) //Ділення всіх значень масиву на набільше значення
+        array[i] /= max;
+
+    return max;
+}
