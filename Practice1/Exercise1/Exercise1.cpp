2d1b9b8a32b5320d44b9eaa5f53f3c1381ece799
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-09-15 22:16:56 +0300

    Pr. 1 Ex. 1, 3 remake

--- Practice1/Exercise1/Exercise1.cpp
+++ Practice1/Exercise1/Exercise1.cpp
@@ -6,11 +6,13 @@
 
 using namespace std;
 
+float V(float);
+float V(float, float);
+
 int main()
 {
     float sphere_radius, cylinder_radius;
     float cylinder_height;
-    float sphere_volume, cylinder_volume;
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
@@ -18,16 +20,22 @@ int main()
     cout << "Введіть радіус сфери: ";
     cin >> sphere_radius;
 
-    sphere_volume = 4.0 / 3.0 * M_PI * pow(sphere_radius, 3);
-
-    cout << endl << "Об'єм сфери = " << fixed << setprecision(2) << sphere_volume << endl;
+    cout << endl << "Об'єм сфери = " << fixed << setprecision(2) << V(sphere_radius) << endl;
 
     cout << endl << "Введіть радіус циліндра: ";
     cin >> cylinder_radius;
     cout << "Введіть висоту циліндра: ";
     cin >> cylinder_height;
 
-    cylinder_volume = M_PI * pow(cylinder_radius, 2) * cylinder_height;
+    cout << endl << "Об'єм циліндра = " << fixed << setprecision(2) << V(cylinder_radius, cylinder_height) << endl;
+}
 
-    cout << endl << "Об'єм циліндра = " << fixed << setprecision(2) << cylinder_volume << endl;
+float V(float sphere_radius) { //Функція для обрахунку об'єму сфери
+    float sphere_volume = 4.0 / 3.0 * M_PI * pow(sphere_radius, 3);
+    return sphere_volume;
+}
+
+float V(float cylinder_radius, float cylinder_height) { //Функція для обрахунку об'єму циліндра
+    float cylinder_volume = M_PI * pow(cylinder_radius, 2) * cylinder_height;
+    return cylinder_volume;
 }
--- Practice1/Exercise3/Exercise3.cpp
+++ Practice1/Exercise3/Exercise3.cpp
@@ -1,27 +1,23 @@
-﻿#define _USE_MATH_DEFINES
-#include <iostream>
-#include <cmath>
+﻿#include <iostream>
 #include <windows.h>
-#include <iomanip>
-#include <string>
 
 using namespace std;
 
-void zarplata(string, int days = 24, float rascenka = 30.5, float premia = 0, float coef_slojnosti = 1.3);
+void zarplata(const char* s, int days = 24, float rascenka = 30.5, float premia = 0, float coef_slojnosti = 1.3);
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    zarplata("Андрій");
-    zarplata("Степан", 26);
-    zarplata("Микола", 24, 41);
-    zarplata("Йосип", 26, 34.5, 400);
-    zarplata("Сергій", 25, 29.8, 350, 1.9);
+    zarplata("Андрій"); //Працював 24 дні по 30.5 грн/день з премією 0 грн. та коеф. праці 1.3
+    zarplata("Степан", 26); //Працював 26 днів по 30.5 грн/день з премією 0 грн. та коеф. праці 1.3
+    zarplata("Микола", 24, 41); //Працював 24 дні по 41 грн/день з премією 0 грн. та коеф. праці 1.3
+    zarplata("Йосип", 26, 34.5, 400); //Працював 26 днів по 34.5 грн/день з премією 400 грн. та коеф. праці 1.3
+    zarplata("Сергій", 25, 29.8, 350, 1.9); //Працював 25 днів по 29.8 грн/день з премією 350 грн. та коеф. праці 1.9
 }
 
-void zarplata(string s, int days, float rascenka, float premia, float coef_slojnosti)
+void zarplata(const char* s, int days, float rascenka, float premia, float coef_slojnosti)
 {
     cout << s << " заробив " << days * rascenka * coef_slojnosti  << " грн." << " та премію " << 
         premia << " грн." << endl;
