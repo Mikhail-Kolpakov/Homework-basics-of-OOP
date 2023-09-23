dbdf693f52599fb804eb4c93971d5f7853efa23a
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-09-23 23:19:59 +0300

    Pr. 10 Ex. 1, 2

---
+++ Practice10/Exercise1/Exercise1.cpp
@@ -0,0 +1,94 @@
+﻿//Варіант #9
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#define MAX_NAME_LENGTH 100
+
+using namespace std;
+
+//Клас, що описує транспорт
+class transport {
+protected:
+    char type[MAX_NAME_LENGTH]; //Тип транспорту
+    float clear_profit; //Чистий прибуток
+    float max_speed; //Максимальна швидкість
+    float weight; //Вага
+
+public:
+    transport(const char* type, float clear_profit, float max_speed, float weight) {
+        strncpy_s(this->type, type, MAX_NAME_LENGTH); //Копіюємо вміст рядка type в масив this->type
+        this->type[MAX_NAME_LENGTH - 1] = '\0'; //Переконуємося, що рядок закінчується нульовим символом
+        this->clear_profit = clear_profit;
+        this->max_speed = max_speed;
+        this->weight = weight;
+    }
+
+    //Створюємо віртуальний метод
+    virtual void output() {
+        cout << "Тип транспорту: " << type << endl;
+        cout << "Чистий прибуток: " << fixed << setprecision(2) << clear_profit << " грн." << endl;
+        cout << "Максимальна швидкість: " << fixed << setprecision(2) << max_speed << " км/год." << endl;
+        cout << "Вага: " << fixed << setprecision(2) << weight << " кг." << endl;
+    }
+
+    virtual ~transport() {} //Створюємо віртуальний деструктор
+};
+
+//Клас, що описує вантажний транспорт
+class cargo_transport : public transport {
+private:
+    float lifting_capacity; //Вантажопідйомність
+
+public:
+    cargo_transport(const char* type, float clear_profit, float max_speed, float weight, float lifting_capacity) : transport(type, clear_profit, max_speed, weight) {
+        this->lifting_capacity = lifting_capacity;
+    }
+
+    //Явним чином вказуємо, що метод перевизначен
+    void output() override {
+        transport::output();
+        cout << "Вантажопідйомність: " << fixed << setprecision(2) << lifting_capacity << " кг." << endl;
+    }
+};
+
+//Клас, що описує пасажирський транспорт
+class passenger_transport : public transport {
+private:
+    int passenger_amount; //Кількість пасажирів
+
+public:
+    passenger_transport(const char* type, float clear_profit, float max_speed, float weight, int passenger_amount) : transport(type, clear_profit, max_speed, weight) {
+        this->passenger_amount = passenger_amount;
+    }
+
+    //Явним чином вказуємо, що метод перевизначен
+    void output() override {
+        transport::output();
+        cout << "Кількість пасажирів: " << passenger_amount << " шт." << endl;
+    }
+};
+
+int main()
+{
+    transport transport_ship("баржа", 45650, 40, 7500);
+    cargo_transport transport_truck("вантажівка", 52000, 105, 15750, 6000);
+    passenger_transport transport_bus("автобус", 31200, 90, 10400, 52);
+    transport* transport(&transport_ship); //Створюємо покажчик та надаємо йому посилання на об'єкт transport_ship
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    transport->output(); //Викликаємо через покажчик метод output
+
+    cout << endl;
+
+    transport = &transport_truck; //Надаємо покажчику посилання на об'єкт transport_truck
+    transport->output();
+
+    cout << endl;
+
+    transport = &transport_bus;
+    transport->output();
+
+    delete transport;
+}
---
+++ Practice10/Exercise2/Exercise2.cpp
@@ -0,0 +1,114 @@
+﻿//Варіант #9
+#include <iostream>
+#include <windows.h>
+#define MAX_NAME_LENGTH 100
+
+using namespace std;
+
+//Клас, що характерихує випробування
+class trial {
+protected:
+    char subject[MAX_NAME_LENGTH]; //Предмет з якого проводиться
+    int grade; //Оцінка
+
+public:
+    trial(const char* subject, int grade) {
+        strncpy_s(this->subject, subject, MAX_NAME_LENGTH); //Копіюємо вміст рядка subject в масив this->subject
+        this->subject[MAX_NAME_LENGTH - 1] = '\0'; //Переконуємося, що рядок закінчується нульовим символом
+        this->grade = grade;
+    }
+
+    //Створюємо віртуальний метод
+    virtual void output() {
+        cout << "Предмет з якого проводиться: " << subject << endl;
+        cout << "Оцінка: " << grade << " балів" << endl;
+    }
+
+    virtual ~trial() {} //Створюємо віртуальний деструктор
+};
+
+//Клас, що характеризує екзамен
+class exam : public trial {
+protected:
+    int duration_min; //Тривалість в хвилинах
+
+public:
+    exam(const char* subject, int grade, int duration_min) : trial(subject, grade) {
+        this->duration_min = duration_min;
+    }
+
+    //Явним чином вказуємо, що метод перевизначен
+    void output() override {
+        trial::output();
+        cout << "Тривалість: " << duration_min << " хв." << endl;
+    }
+};
+
+//Клас, що характеризує випускний екзамен
+class final_exam : public exam {
+private:
+    int passing_grade; //Прохідний бал
+
+public:
+    final_exam(const char* subject, int grade, int duration_min, int passing_grade) : exam(subject, grade, duration_min) {
+        this->passing_grade = passing_grade;
+    }
+
+    //Явним чином вказуємо, що метод перевизначен
+    void output() override {
+        exam::output();
+        cout << "Прохідний бал: " << passing_grade << " балів" << endl;
+    }
+};
+
+//Клас, що характеризує тест
+class test : public trial {
+private:
+    char topic[MAX_NAME_LENGTH]; //Тема тесту
+    int questions_number; //Кількість питань
+
+public:
+    test(const char* subject, int grade, const char* topic, int questions_number) : trial(subject, grade) {
+        strncpy_s(this->topic, topic, MAX_NAME_LENGTH);
+        this->topic[MAX_NAME_LENGTH - 1] = '\0';
+        this->questions_number = questions_number;
+    }
+
+    //Явним чином вказуємо, що метод перевизначен
+    void output() override {
+        trial::output();
+        cout << "Тема тесту: " << topic << endl;
+        cout << "Кількість питань: " << questions_number << " шт." << endl;
+    }
+};
+
+int main()
+{
+    trial biology_trial("Біологія", 10);
+    exam english_exam("Англійска мова", 8, 25);
+    final_exam algebra_final_exam("Алгебра", 11, 40, 10);
+    test history_test("Історія України", 8, "Україна в часи другої світової війни", 24);
+    trial* trial(&biology_trial); //Створюємо покажчик та надаємо йому посилання на об'єкт biology_trial
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    trial->output(); //Викликаємо через покажчик метод output
+
+    cout << endl;
+
+    trial = &english_exam; //Надаємо покажчику посилання на об'єкт english_exam
+    trial->output();
+
+    cout << endl;
+
+    trial = &algebra_final_exam;
+    trial->output();
+
+    cout << endl;
+
+    trial = &history_test;
+    trial->output();
+
+    delete trial;
+}
