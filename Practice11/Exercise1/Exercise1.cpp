00368bcf7fa8c419ed15cf8fe7da831a72160fd6
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-09-24 22:12:51 +0300

    Pr. 11 Ex. 1, 2

---
+++ Practice11/Exercise1/Exercise1.cpp
@@ -0,0 +1,102 @@
+﻿//Варіант #9
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#define MAX_NAME_LENGTH 100
+
+using namespace std;
+
+//Абстрактний клас, що характеризує корабель
+class ship {
+protected:
+    char name[MAX_NAME_LENGTH]{}; //Назва корабля
+    char belong_to_country[MAX_NAME_LENGTH]{}; //Якій країні належить
+    float weight{}; //Вага
+
+public:
+    //Оголошуємо чисті віртуальні функції
+    virtual void input() = 0;
+    virtual void output() const = 0;
+
+    virtual ~ship() {} //Створюємо віртуальний деструктор
+};
+
+//Клас, що характеризує ректний катер
+class missile_boat : public ship {
+private:
+    int missiles_number{}; //Кількість ракет на борту
+
+public:
+    void input() override { //Функція для введення даних
+        cout << "Введіть дані для ракетного катера: " << endl;
+        cout << "Назва: ";
+        cin.getline(name, sizeof(name));
+        cout << "Країна якій належить: ";
+        cin.getline(belong_to_country, sizeof(belong_to_country));
+        cout << "Вага: ";
+        cin >> weight;
+        cout << "Кількість ракет на борту: ";
+        cin >> missiles_number;
+
+        cin.ignore(); //Очищюємо символ нового рядка із буфера
+    }
+
+    void output() const override { //Функція для виводу даних
+        cout << "Інформація щодо ракетного катера: " << endl;
+        cout << "Назва: " << name << endl;
+        cout << "Країна якій належить: " << belong_to_country << endl;
+        cout << "Вага: " << fixed << setprecision(2) << weight << " т." << endl;
+        cout << "Кількість ракет на борту: " << missiles_number << " шт." << endl;
+    }
+};
+
+//Клас, що характеризує пасажирський лайнер
+class passenger_boat : public ship {
+private:
+    int passengers_capacity{}; //Пасажиромісткість
+
+public:
+    void input() override { //Функція для введення даних
+        cout << "Введіть дані для пасажирського лайнера: " << endl;
+        cout << "Назва: ";
+        cin.getline(name, sizeof(name));
+        cout << "Країна якій належить: ";
+        cin.getline(belong_to_country, sizeof(belong_to_country));
+        cout << "Вага: ";
+        cin >> weight;
+        cout << "Пасажиромісткість: ";
+        cin >> passengers_capacity;
+
+        cin.ignore(); //Очищюємо символ нового рядка із буфера
+    }
+
+    void output() const override { //Функція для виводу даних
+        cout << "Інформація щодо ракетного катера: " << endl;
+        cout << "Назва: " << name << endl;
+        cout << "Країна якій належить: " << belong_to_country << endl;
+        cout << "Вага: " << fixed << setprecision(2) << weight << " т." << endl;
+        cout << "Пасажиромісткість: " << passengers_capacity << " шт." << endl;
+    }
+};
+
+int main()
+{
+    ship* ship = new missile_boat(); //Створюємо покажчик та надаємо йому посилання на об'єкт missile_boat
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ship->input();
+    cout << endl;
+    ship->output();
+
+    cout << endl << endl;
+
+    ship = new passenger_boat();
+    ship->input();
+    cout << endl;
+    ship->output();
+
+    delete ship;
+    ship = nullptr;
+}
---
+++ Practice11/Exercise2/Exercise2.cpp
@@ -0,0 +1,129 @@
+﻿//Варіант #9
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#define MAX_NAME_LENGTH 100
+
+using namespace std;
+
+//Абстрактний клас, що характеризує співробітників
+class employee {
+protected:
+    char name[MAX_NAME_LENGTH]; //Ім'я
+    float salary; //Зарплатня
+
+public:
+    employee(const char* name, float salary) {
+        strncpy_s(this->name, name, MAX_NAME_LENGTH); //Копіюємо вміст рядка name в масив this->name
+        this->name[MAX_NAME_LENGTH - 1] = '\0'; //Переконуємося, що рядок закінчується нульовим символом
+        this->salary = salary;
+    }
+
+    //Оголошуємо чисті віртуальні функції
+    virtual float calculate_salary() const = 0;
+    virtual void output() const = 0;
+
+    virtual ~employee() {} //Створюємо віртуальний деструктор
+};
+
+//Клас, що характеризує кадрів
+class personnel : public employee {
+public:
+    personnel(const char* name, float salary) : employee(name, salary) {}
+
+    float calculate_salary() const override { //Функція для розрахування зарплатні
+        return salary;
+    }
+
+    void output() const override { //Функція для виводу даних
+        cout << "Кадри: " << name << endl;
+        cout << "Зарплатня всіх разом: " << fixed << setprecision(2) << calculate_salary() << " грн." << endl;
+    }
+};
+
+//Клас, що характеризує робітника
+class worker : public personnel {
+private:
+    const float difficulty_factor = 1.3; //Коефіцієнт складності
+
+public:
+    worker(const char* name, float salary) : personnel(name, salary) {}
+
+    float calculate_salary() const override { //Функція для розрахування зарплатні
+        float final_salary = salary * difficulty_factor;
+        return final_salary;
+    }
+
+    void output() const override { //Функція для виводу даних
+        cout << "Робітник: " << name << endl;
+        cout << "Зарплатня: " << fixed << setprecision(2) << calculate_salary() << " грн." << endl;
+    }
+};
+
+//Клас, що характеризує інженера
+class engineer : public personnel {
+private:
+    const float difficulty_factor = 1.9; //Коефіцієнт складності
+    const int premium = 570; //Премія
+
+public:
+    engineer(const char* name, float salary) : personnel(name, salary) {}
+
+    float calculate_salary() const override { //Функція для розрахування зарплатні
+        float final_salary = salary * difficulty_factor + premium;
+        return final_salary;
+    }
+
+    void output() const override { //Функція для виводу даних
+        cout << "Інженер: " << name << endl;
+        cout << "Зарплатня: " << fixed << setprecision(2) << calculate_salary() << " грн." << endl;
+    }
+};
+
+//Клас, що характеризує адміністрацію
+class administration : public personnel {
+private:
+    const float difficulty_factor = 1.8; //Коефіцієнт складності
+    const int premium = 1000; //Премія
+
+public:
+    administration(const char* name, float salary) : personnel(name, salary) {}
+
+    float calculate_salary() const override { //Функція для розрахування зарплатні
+        float final_salary = salary * difficulty_factor + premium;
+        return final_salary;
+    }
+
+    void output() const override { //Функція для виводу даних
+        cout << "Адміністратор: " << name << endl;
+        cout << "Зарплатня: " << fixed << setprecision(2) << calculate_salary() << " грн." << endl;
+    }
+};
+
+int main()
+{
+    employee* employee = new personnel("Андрій, Богдан, Захарій, Сергій", 88650); //Створюємо покажчик та надаємо йому посилання на об'єкт personnel
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    employee->output();
+
+    cout << endl;
+
+    employee = new worker("Валерій", 9840);
+    employee->output();
+
+    cout << endl;
+
+    employee = new engineer("Іван", 12500);
+    employee->output();
+
+    cout << endl;
+
+    employee = new administration("Ольга", 11230);
+    employee->output();
+
+    delete employee;
+    employee = nullptr;
+}
