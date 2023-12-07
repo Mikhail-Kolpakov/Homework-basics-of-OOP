dffcb0c6fe74c30b7a928f88a2bf60c0804b06bd
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-12-07 21:23:38 +0200

    CPC2 some small naming changes

--- CPC2/CPC2.cpp
+++ CPC2/CPC2.cpp
@@ -35,17 +35,17 @@ int main()
 	int int_array[] = { 0, 2, 3, 4, 3, 6 };
 	int int_value = 5;
 	element_counter<int> int_counter(int_array);
-	auto int_count = int_counter.count_elements_greater_then<int, sizeof(int_array) / sizeof(int_array[0])>(int_value);
+	auto count1 = int_counter.count_elements_greater_then<int, sizeof(int_array) / sizeof(int_array[0])>(int_value);
 
-	cout << "Кількість елементів більших за " << int_value << ": " << int_count << endl;
-	cout << "Функція отримує параметр int_value типу " << (typeid(int_value).name()) << " та повертає значення int_count типу " << (typeid(int_count).name()) << endl << endl;
+	cout << "Кількість елементів більших за " << int_value << ": " << count1 << endl;
+	cout << "Функція отримує параметр int_value типу " << (typeid(int_value).name()) << " та повертає значення count1 типу " << (typeid(count1).name()) << endl << endl;
 
 	// Перевіряємо для типу даних float
 	float float_array[] = { 0.5F, 2.3F, 3.7F, 4.1F, 3.8F, 6.2F };
 	float float_value = 3.0F;
 	element_counter<float> float_counter(float_array);
-	auto float_count = float_counter.count_elements_greater_then<float, sizeof(int_array) / sizeof(int_array[0])>(float_value);
+	auto count2 = float_counter.count_elements_greater_then<float, sizeof(float_array) / sizeof(float_array[0])>(float_value);
 
-	cout << "Кількість елементів більших за " << float_value << ": " << float_count << endl;
-	cout << "Функція отримує параметр float_value типу " << (typeid(float_value).name()) << " та повертає значення float_count типу " << (typeid(float_count).name()) << endl;
+	cout << "Кількість елементів більших за " << float_value << ": " << count2 << endl;
+	cout << "Функція отримує параметр float_value типу " << (typeid(float_value).name()) << " та повертає значення count2 типу " << (typeid(count2).name()) << endl;
 }
