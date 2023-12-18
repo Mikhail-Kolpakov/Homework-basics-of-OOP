e39499f73f987883306de85df355b88b1d5a93f2
Mikhail-Kolpakov <mihailkolp@gmail.com>
2023-12-18 17:14:15 +0200

    Control_Work3

---
+++ Control_Work3/Exercise1/Exercise1.cpp
@@ -0,0 +1,110 @@
+﻿// Варіант #9
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+using namespace std;
+
+// Клас, що характеризує вектор
+class vector {
+private:
+    float* array; // Вектор - одновимірний масив
+    int size; // Кількість елементів вектора
+
+public:
+    vector() : array(nullptr), size(0) {}
+
+    ~vector() {
+        delete[] array;
+    }
+
+    int count_elements_less_than_average() const { // Метод для обчислення кількості елементів менших за середнє 
+        if (size == 0) // Якщо вектор порожній, повертаємо 0
+            return 0;
+
+        float sum = 0;
+
+        for (int i = 0; i < size; i++) // Рахуємо суму всії елементів вектору
+            sum += array[i];
+
+        float average = sum / size; // Вираховуємо середнє значення
+        int count = 0;
+
+        for (int i = 0; i < size; i++) // Знаходимо кількість елементів менших за середнє значення
+            if (array[i] < average)
+                count++;
+
+        return count;
+    }
+
+    friend istream& operator >> (istream& stream, vector& vec) { //Читаємо дані з потоку
+        stream >> vec.size;
+
+        vec.array = new float[vec.size];
+
+        for (int i = 0; i < vec.size; i++)
+            stream >> vec.array[i];
+
+        return stream;
+    }
+
+    friend ostream& operator << (ostream& stream, const vector& vec) { //Записуємо дані в потік
+        for (int i = 0; i < vec.size; i++)
+            stream << vec.array[i] << " ";
+
+        return stream;
+    }
+
+    vector operator * (float value) const { // Перевантажуємо операцію *
+        vector result;
+        result.size = size;
+        result.array = new float[size];
+
+        for (int i = 0; i < size; i++)
+            result.array[i] = array[i] * value;
+
+        return result;
+    }
+
+    vector operator -= (const vector& other) { // Перевантажуємо операцію -=
+        for (int i = 0; i < size; i++)
+            array[i] -= other.array[i];
+
+        return *this;
+    }
+};
+
+int main()
+{
+    vector A, B, C; // Вектори
+    float R; // Дійсне число
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ifstream input_file("input.txt"); // Відкриваємо файл(за допомогою конструктора) для зчитування
+
+    if (!input_file.is_open()) { // Якщо файл не відкрився
+        cout << "Не вдалося відкрити файл input.txt для зчитування" << endl;
+        exit(1);
+    }
+
+    input_file >> A >> R;
+    input_file.close(); // Закриваємо файл
+
+    C = A * R;
+    B -= C;
+
+    int count = C.count_elements_less_than_average();
+
+    ofstream output_file("output.txt"); // Відкриваємо файл(за допомогою конструктора) для запису
+
+    if (!output_file.is_open()) { // Якщо файл не відкрився
+        cout << "Не вдалося відкрити файл output.txt для запису" << endl;
+        exit(1);
+    }
+
+    output_file << C;
+    output_file << "\nКількість елементів менших за середнє: " << count;
+    output_file.close(); // Закриваємо файл
+}
---
+++ Control_Work3/Exercise2/Exercise2.cpp
@@ -0,0 +1,72 @@
+﻿// Варіант #9
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+float function(float delta_x, float a, float b, float c) { // Функція задана за умовою завдання
+    // Робимо перевірки на допустимість вхідних значень для функції заданою мені за умовою
+    if (c * delta_x + pow(a, 2.0) < 0) {
+        cout << "Підкореневий вираз не може приймати значення менше 0" << endl;
+        cout << "delta_x = " << delta_x << ", a = " << a << ", b = " << b << ", c = " << c << endl << endl;
+        return -INFINITY;
+    }
+    if (b <= 0) {
+        cout << "Функція натурального логорифму може приймати тільки додатні значення" << endl;
+        cout << "delta_x = " << delta_x << ", a = " << a << ", b = " << b << ", c = " << c << endl << endl;
+        return -INFINITY;
+    }
+    if (log(b) == 0) {
+        cout << "Знаменник при діленні не може дорівнювати 0" << endl;
+        cout << "delta_x = " << delta_x << ", a = " << a << ", b = " << b << ", c = " << c << endl << endl;
+        return -INFINITY;
+    }
+
+    return (sqrt(c * delta_x + pow(a, 2.0)) + 5) / (log(b)) + sin(a); // Повертаємо результат розрахунків функції заданою мені за умовою 
+}
+
+int main()
+{
+    float x_min, x_max, x_delta;
+    float a, b, c;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ifstream input_file("input.txt"); // Відкриваємо файл(за допомогою конструктора) для зчитування
+
+    if (!input_file.is_open()) { // Якщо файл не відкрився
+        cout << "Не вдалося відкрити файл input.txt для зчитування" << endl;
+        exit(1);
+    }
+
+    // Зчитуємо дані
+    input_file >> x_min >> x_max >> x_delta;
+    input_file >> a >> b >> c;
+
+    input_file.close(); // Закриваємо файл
+
+    ofstream output_file("output.txt"); // Відкриваємо файл(за допомогою конструктора) для запису
+
+    if (!output_file.is_open()) { // Якщо файл не відкрився
+        cout << "Не вдалося відкрити файл output.txt для запису" << endl;
+        exit(1);
+    }
+
+    output_file << "Початкові значення для a, b, c відповідно: " << a << ", " << b << ", " << c << endl << endl;
+    output_file << setw(15) << "Значення x" << setw(25) << "Значення функції" << endl;
+    
+    for (float x = x_min; x <= x_max; x += x_delta) { // Робимо основні розрахунки
+        float result = function(x, a, b, c);
+
+        if (result == -INFINITY)
+            output_file << "Недопустимі значення вхідних параметрів на даному кроці" << endl;
+        else
+            output_file << setw(15) << x << setw(25) << result << endl;
+    }
+
+    output_file.close(); // Закриваємо файл
+}
